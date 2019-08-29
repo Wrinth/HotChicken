6de028a340ac78e809212fed53acfb745b947eed
Wrinth <johnl4112@gmail.com>
2019-08-29 16:04:12 +0800

    First commit

---
+++ download_dataset.py
@@ -0,0 +1,39 @@
+import urllib
+import os
+import requests
+
+# Open the mapping_list file with read only permit
+f = open('./train/mapping_list.txt')
+# use readline() to read the first line 
+line = f.readline()
+while line:
+    name, type1, type2 = line.split(',')
+    directory = 'dataset/'+type2
+    if not os.path.exists(directory.rstrip()):
+        os.makedirs(directory.rstrip())
+    else:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                os.remove(os.path.join(root, file))
+    line = f.readline()
+f.close()
+
+# Open the train file with read only permit
+f = open('./train/train.txt')
+# use readline() to read the first line 
+line = f.readline()
+while line:
+    url, type1, type2 = line.rsplit(',', 2)
+    directory = 'dataset/' + type2
+
+    onlyfiles = next(os.walk(directory.rstrip()))[2]
+    filename = format(int(type2), '03d') + '_' + format(len(onlyfiles), '05d') + '.jpg'
+
+    try:
+        r = requests.get(url, allow_redirects=True)
+        open('./' + directory.rstrip() + '/' + filename, 'wb').write(r.content)
+    except requests.RequestException as e:
+        print(e)
+
+    line = f.readline()
+f.close()
